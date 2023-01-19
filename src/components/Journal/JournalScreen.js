9ac47edee8a05d348e10face4d78c3321c697a0c
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-01-19 00:10:49 -0600

    first commit

---
+++ src/components/Journal/JournalScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from './SideBar'
+
+const JournalScreen = () => {
+  return (
+    <div className='journal__main-content'> 
+        <SideBar/>
+        <main>
+          <h1>Main content</h1>
+        </main>
+    </div>
+  )
+}
+
+export default JournalScreen
---
+++ src/router/AppRouter.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+  } from "react-router-dom";
+import AuthRouter from './AuthRouter';
+import JournalScreen from '../components/Journal/JournalScreen';
+const AppRouter = () => {
+  return (
+    <Router>
+        <div>
+            <Switch>
+                <Route exact path='/' component={JournalScreen}/>
+                <Route path='/auth' component={AuthRouter}/>
+                <Redirect to='/auth/login' />
+            </Switch>
+        </div>
+    </Router>
+  )
+}
+
+export default AppRouter
